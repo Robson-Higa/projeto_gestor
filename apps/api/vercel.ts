5f26d6595e20d5041cf07edeeb394f3a6d4713b4
Robson-Higa <higarobson@gmail.com>
2025-08-18 20:15:01 -0400

    erro de inicializacao

---
+++ apps/api/vercel.ts
@@ -0,0 +1,10 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import app from './index';
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  // Configuração específica para o Vercel
+  if (req.url?.startsWith('/api')) {
+    req.url = req.url.replace('/api', '');
+  }
+  return app(req, res);
+};
